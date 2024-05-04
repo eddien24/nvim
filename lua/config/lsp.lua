1c36cb3d072167b2c2073a9123d45c158bdbcba8
Eddie Nguyen <edward_nguyen@brown.edu>
2024-05-04 16:19:03 -0400

    conform & mason

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,17 +2,35 @@ local lsp_zero = require('lsp-zero')
 local config = require('lspconfig')
 local cmp = require('cmp')
 
-lsp_zero.on_attach(function(client, bufnr)
-    lsp_zero.default_keymaps({buffer = bufnr})
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "rust_analyzer"
+    }
+})
+
+lsp_zero.on_attach(function(_, bufnr)
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-config.clangd.setup{}
-config.cmake.setup{}
-config.rust_analyzer.setup{}
+config.lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
+        }
+    }
+}
+
+config.clangd.setup {}
+config.rust_analyzer.setup {}
 
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }),
     preselect = 'item',
     completion = {
