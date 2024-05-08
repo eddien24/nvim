869cac37a593e3e70db87b4bf85ced153730ee4d
Eddie Nguyen <edward_nguyen@brown.edu>
2024-05-08 18:35:07 -0400

    telescope buffers & mason for formatting

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,44 +1,51 @@
-local lsp_zero = require('lsp-zero')
-local config = require('lspconfig')
-local cmp = require('cmp')
+local lsp_zero = require("lsp-zero")
+local config = require("lspconfig")
+local cmp = require("cmp")
 
-require("mason").setup()
+require("mason").setup({
+    ensure_installed = {
+        -- formatters
+        "clang-format",
+    },
+})
+
 require("mason-lspconfig").setup({
     ensure_installed = {
+        -- LSPs
         "lua_ls",
         "clangd",
-        "rust_analyzer"
-    }
+        "rust_analyzer",
+    },
 })
 
 lsp_zero.on_attach(function(_, bufnr)
     lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-config.lua_ls.setup {
+config.lua_ls.setup({
     settings = {
         Lua = {
             diagnostics = {
-                globals = { "vim" }
-            }
-        }
-    }
-}
-
-config.clangd.setup {
-    init_options = {
-        fallbackFlags = { "--std=c++20" }
+                globals = { "vim" },
+            },
+        },
     },
-}
+})
 
-config.rust_analyzer.setup {}
+config.clangd.setup({
+    init_options = {
+        fallbackFlags = { "--std=c++20" },
+    },
+})
+
+config.rust_analyzer.setup({})
 
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
     }),
-    preselect = 'item',
+    preselect = "item",
     completion = {
-        completeopt = 'menu,menuone,noinsert'
+        completeopt = "menu,menuone,noinsert",
     },
 })
