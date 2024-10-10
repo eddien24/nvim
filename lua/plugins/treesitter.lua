23eb5cb64491179be5db4bc4eede9bb23b68ef81
Eddie Nguyen <edward_nguyen@brown.edu>
2024-10-10 14:34:16 -0400

    treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,18 +3,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require 'nvim-treesitter.configs'.setup {
-            ensure_installed = {
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-                "cmake",
-                "cpp",
-                "rust",
-                "hyprlang",
-                "latex",
-            },
+            ensure_installed = "all",
             sync_install = false,
             auto_install = true,
             highlight = {
