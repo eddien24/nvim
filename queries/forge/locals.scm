0354d9625e70f9b23859c16cef01f2ff2f106eb9
enguye49 <enguye49@node0.enguye49-310189.ccp-pg0.cloudlab.umass.edu>
2026-07-08 11:09:05 -0600

    rm forge

--- queries/forge/locals.scm
+++
@@ -1,7 +0,0 @@
-(sig_declaration (identifier) @definition.type)
-(field_declaration (identifier) @definition.field)
-(pred_declaration (identifier) @definition.function)
-(fun_declaration (identifier) @definition.function)
-(parameter (identifier) @definition.parameter)
-
-(identifier) @reference
